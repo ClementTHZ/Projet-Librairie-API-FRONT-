a6afb2528747fd64f5206de960d67206f75f6135
Clément THOREZ <cthorez@selectour.fr>
2025-05-14 17:45:13 +0200

    make request add quantity

---
+++ FRONTEND/pages/bookDetail.js
@@ -0,0 +1,42 @@
+const params = new URLSearchParams(window.location.search); // = params de l'URL
+const bookId = params.get("id"); 
+
+async function addQuantity(id) {
+    const url = `http://localhost:5183/books/${id}`
+    const response = await fetch(url, {
+        method: "PUT"
+    }); 
+    console.log(response.status)
+}
+
+async function bookDetails(id){
+    const url = `http://localhost:5183/books/${id}`; 
+    try {
+        const response = await fetch(url); 
+        if(!response.ok) throw new Error(`Response Status: ${response.status}`); 
+        const data = await response.json(); 
+
+        document.getElementById("book-title").textContent = data.title; 
+        document.getElementById("book-description").textContent = data.description; 
+        document.getElementById("book-author").textContent = `Auteur: ${data.author}`;
+        const h3 = document.getElementById("book-quantity"); 
+        h3.textContent = `Quantité: ${data.quantity}`; 
+
+        const a = document.createElement("a"); 
+        a.href=""; 
+        const addQuantityButton = document.createElement("i");
+        addQuantityButton.classList.add("fa-solid");
+        addQuantityButton.classList.add("fa-plus");
+        addQuantityButton.addEventListener("click", () => {
+            addQuantity(bookId); 
+            location.reload; 
+        }); 
+
+        a.appendChild(addQuantityButton)
+        h3.appendChild(a); 
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+bookDetails(bookId); 
--- FRONTEND/pages/bookList.js
+++ FRONTEND/pages/bookList.js
@@ -8,13 +8,47 @@ async function getAllBooks() {
 
         const bookList = document.getElementById("book-list")
         data.forEach(book => {
+            const bookId = book.id
+
+            const link = document.createElement("a")
+            link.href = `./bookDetail.html?id=${bookId}`
+
             const li = document.createElement("li")
-            li.textContent = `${book.title}`
-            bookList.appendChild(li)
+            li.textContent = `${book.title} / auteur: ${book.author} `
+
+            const deleteTrash = document.createElement("i"); 
+            deleteTrash.classList.add("fa-solid"); 
+            deleteTrash.classList.add("fa-trash");
+            deleteTrash.addEventListener("click", () => {
+                deleteBook(bookId)
+            }); 
+            const a = document.createElement("a"); 
+            a.href = ""; 
+
+            a.appendChild(deleteTrash)
+            li.appendChild(a)
+            link.appendChild(li)
+            bookList.appendChild(link)
         });
     } catch (error) {
         console.log(error.message)
     }
 }
+
+async function deleteBook(bookIdId) {
+    const url = `http://localhost:5183/books/${bookId}`; 
+    try {
+        const response = await fetch(url, {
+        method: "DELETE", 
+        }); 
+        console.log(response.status); 
+        location.reload; 
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+// Faire une fonction getBookbyId qui récupère l'id du livre cliquer
+// Faire une fonction
 
 getAllBooks(); 
